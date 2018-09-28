67c01faca6c85a6207224dc856126e5d48658cfc
t0404mnl <majara@stefanini.com>
2018-09-28 15:07:06 -0300

    add js

--- js/versiculos.js
+++ js/versiculos.js
@@ -1,34 +1,28 @@
-let versiculosDropdown = document.getElementById('locality-versiculosDropdown');
-versiculosDropdown.length = 0;
+function onBuscarVersiculo(){
+	console.log("onBuscarVersiculo");
 
-let defaultOptionVersiculos = document.createElement('option');
-defaultOptionVersiculos.text = 'Choose State/Province';
+	let lblVersiculo = document.getElementById('lbl-versiculo');
+	var vers = document.getElementById('versoes-dropdown').value
+	var test = document.getElementById('testamentos-dropdown').value
+	var liv = document.getElementById('livros-dropdown').value
 
-versiculosDropdown.add(defaultOptionVersiculos);
-versiculosDropdown.selectedIndex = 0;
+	const urlVersiculos = 'http://www.mhj.kinghost.net:21137/versiculos/versiculo/' + vers + '/' liv + '/4/8';
 
-const urlVersiculos = 'https://api.myjson.com/bins/7xq2x';
+	const requestVersiculos = new XMLHttpRequest();
+	requestVersiculos.open('GET', urlVersiculos, true);
 
-const requestVersiculos = new XMLHttpRequest();
-requestVersiculos.open('GET', urlVersiculos, true);
+	requestVersiculos.onload = function() {
+	  if (requestVersiculos.status === 200) {
+		const data = JSON.parse(requestVersiculos.responseText);
+		lblVersiculo.innerHTML = data[i].ver_texto;
+	   } else {
+		// Reached the server, but it returned an error
+	  }   
+	}
 
-requestVersiculos.onload = function() {
-  if (requestVersiculos.status === 200) {
-    const data = JSON.parse(requestVersiculos.responseText);
-    let option;
-    for (let i = 0; i < data.length; i++) {
-      option = document.createElement('option');
-      option.text = data[i].name;
-      option.value = data[i].abbreviation;
-      versiculosDropdown.add(option);
-    }
-   } else {
-    // Reached the server, but it returned an error
-  }   
+	requestVersiculos.onerror = function() {
+	  console.error('An error occurred fetching the JSON from ' + urlVersiculos);
+	};
+
+	requestVersiculos.send();
 }
-
-requestVersiculos.onerror = function() {
-  console.error('An error occurred fetching the JSON from ' + urlVersiculos);
-};
-
-requestVersiculos.send();
