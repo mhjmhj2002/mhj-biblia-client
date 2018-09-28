e9039af36427bec7fdec05d3dbb3a0b5b7fcfe7c
t0404mnl <majara@stefanini.com>
2018-09-28 15:11:21 -0300

    add js

--- js/versiculos.js
+++ js/versiculos.js
@@ -6,7 +6,7 @@ function onBuscarVersiculo(){
 	var test = document.getElementById('testamentos-dropdown').value
 	var liv = document.getElementById('livros-dropdown').value
 
-	const urlVersiculos = 'http://www.mhj.kinghost.net:21137/versiculos/versiculo/' + vers + '/' liv + '/4/8';
+	const urlVersiculos = 'http://www.mhj.kinghost.net:21137/versiculos/versiculo/' + vers + '/' + liv + '/4/8';
 
 	const requestVersiculos = new XMLHttpRequest();
 	requestVersiculos.open('GET', urlVersiculos, true);
