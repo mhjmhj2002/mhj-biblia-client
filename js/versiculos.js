ba7a85b2d2f2844f4c8322a524512cff1218e9f2
mhjmhj2002 <mhjmhj2002@gmail.com>
2018-09-30 19:00:44 -0300

    add query

--- js/versiculos.js
+++ js/versiculos.js
@@ -6,7 +6,7 @@ function onBuscarVersiculo(){
 	var cap = document.getElementById('inp-capitulo').value;
 	var ver = document.getElementById('inp-versiculo').value;
 
-	const urlVersiculos = 'http://www.mhj.kinghost.net:21137/versiculos/versiculo/' + vers + '/' + liv + '/' + cap '/' + ver;
+	const urlVersiculos = 'http://www.mhj.kinghost.net:21137/versiculos/versiculo/' + vers + '/' + liv + '/' + cap + '/' + ver;
 
 	const requestVersiculos = new XMLHttpRequest();
 	requestVersiculos.open('GET', urlVersiculos, true);
