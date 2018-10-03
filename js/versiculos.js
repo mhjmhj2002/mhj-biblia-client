ef149174e3c430cbec7ec8899e6956e2af75f02d
t0404mnl <majara@stefanini.com>
2018-10-03 16:19:16 -0300

    add ajuste

--- js/versiculos.js
+++ js/versiculos.js
@@ -1,4 +1,4 @@
-function onBuscarVersiculo(){
+function onBuscarVersiculo() {
 	console.log("onBuscarVersiculo");
 
 	var vers = document.getElementById('versoes-dropdown').value;
@@ -12,41 +12,41 @@ function onBuscarVersiculo(){
 	const requestVersiculos = new XMLHttpRequest();
 	requestVersiculos.open('GET', urlVersiculos, true);
 
-	requestVersiculos.onload = function() {
-	  if (requestVersiculos.status === 200) {
-		const data = JSON.parse(requestVersiculos.responseText);
-		var result = "";
-		for (let i = 0; i < data.length; i++) {
-			console.log(data[i].ver_texto);	
-            result += " ";
-            result += data[i].ver_versiculo;
-            result += " ";
-			result += data[i].ver_texto;
+	requestVersiculos.onload = function () {
+		if (requestVersiculos.status === 200) {
+			const data = JSON.parse(requestVersiculos.responseText);
+			var result = "";
+			for (let i = 0; i < data.length; i++) {
+				console.log(data[i].ver_texto);
+				result += " ";
+				result += data[i].ver_versiculo;
+				result += " ";
+				result += data[i].ver_texto;
+			}
+			document.getElementById('lbl-versiculo').innerHTML = result;//data[0].ver_texto;
+		} else {
+			// Reached the server, but it returned an error
 		}
-		document.getElementById('lbl-versiculo').innerHTML = result;//data[0].ver_texto;
-	   } else {
-		// Reached the server, but it returned an error
-	  }   
 	}
 
-	requestVersiculos.onerror = function() {
-	  console.error('An error occurred fetching the JSON from ' + urlVersiculos);
+	requestVersiculos.onerror = function () {
+		console.error('An error occurred fetching the JSON from ' + urlVersiculos);
 	};
 
 	requestVersiculos.send();
 }
 
-function onGetQuantidadeCapitulos(){
+function onGetQuantidadeCapitulos() {
 	console.log("onGetQuantidadeCapitulos");
-    
-    let capitulosDropdown = document.getElementById('capitulos-dropdown');
-    capitulosDropdown.length = 0;
-    let defaultOptionCapitulos = document.createElement('option');
-    defaultOptionCapitulos.text = 'Escolha um item';
-          
-    capitulosDropdown.add(defaultOptionCapitulos);
-    capitulosDropdown.selectedIndex = 0;
-	
+
+	let capitulosDropdown = document.getElementById('capitulos-dropdown');
+	capitulosDropdown.length = 0;
+	let defaultOptionCapitulos = document.createElement('option');
+	defaultOptionCapitulos.text = 'Escolha um item';
+
+	capitulosDropdown.add(defaultOptionCapitulos);
+	capitulosDropdown.selectedIndex = 0;
+
 	var liv = document.getElementById('livros-dropdown').value;
 
 	const urlCapitulos = 'http://www.mhj.kinghost.net:21137/versiculos/capitulo/' + liv;
@@ -54,78 +54,79 @@ function onGetQuantidadeCapitulos(){
 	const requestCapitulos = new XMLHttpRequest();
 	requestCapitulos.open('GET', urlCapitulos, true);
 
-	requestCapitulos.onload = function() {
-	  if (requestCapitulos.status === 200) {
-          
-          const data = JSON.parse(requestCapitulos.responseText);		
-		  console.log(data[0].qtde_capitulos);
-          var qtdeCapitulos = data[0].qtde_capitulos;
-          let option; 
-          
-          for (let i = 1; i <= qtdeCapitulos; i++) {
-              option = document.createElement('option');
-              option.text = i;
-              option.value = i;
-              capitulosDropdown.add(option);
-          }
-	   } else {
-		// Reached the server, but it returned an error
-	  }   
+	requestCapitulos.onload = function () {
+		if (requestCapitulos.status === 200) {
+
+			const data = JSON.parse(requestCapitulos.responseText);
+			console.log(data[0].qtde_capitulos);
+			var qtdeCapitulos = data[0].qtde_capitulos;
+			let option;
+
+			for (let i = 1; i <= qtdeCapitulos; i++) {
+				option = document.createElement('option');
+				option.text = i;
+				option.value = i;
+				capitulosDropdown.add(option);
+			}
+		} else {
+			// Reached the server, but it returned an error
+		}
 	}
 
-	requestCapitulos.onerror = function() {
-	  console.error('An error occurred fetching the JSON from ' + urlCapitulos);
+	requestCapitulos.onerror = function () {
+		console.error('An error occurred fetching the JSON from ' + urlCapitulos);
 	};
 
 	requestCapitulos.send();
 }
 
-function onGetQuantidadeVersiculos(){
+function onGetQuantidadeVersiculos() {
 	console.log("onGetQuantidadeVersiculos");
-    
-		let versiculosDeDropdown = document.getElementById('versiculos-de-dropdown');
-		let versiculosAteDropdown = document.getElementById('versiculos-ate-dropdown');
 
-		versiculosDeDropdown.length = 0;		
-    versiculosAteDropdown.length = 0;
+	let versiculosDeDropdown = document.getElementById('versiculos-de-dropdown');
+	let versiculosAteDropdown = document.getElementById('versiculos-ate-dropdown');
 
-    let defaultOptionVersiculos = document.createElement('option');
-    defaultOptionVersiculos.text = 'Escolha um item';
-          
-    versiculosDeDropdown.add(defaultOptionVersiculos);
-		versiculosDeDropdown.selectedIndex = 0;
-		versiculosAteDropdown.add(defaultOptionVersiculos);
-    versiculosAteDropdown.selectedIndex = 0;
-	
+	versiculosDeDropdown.length = 0;
+	versiculosAteDropdown.length = 0;
+
+	let defaultOptionVersiculos = document.createElement('option');
+	defaultOptionVersiculos.text = 'Escolha um item';
+
+	versiculosDeDropdown.add(defaultOptionVersiculos);
+	versiculosDeDropdown.selectedIndex = 0;
+	versiculosAteDropdown.add(defaultOptionVersiculos);
+	versiculosAteDropdown.selectedIndex = 0;
+
+	var liv = document.getElementById('livros-dropdown').value;
 	var cap = document.getElementById('capitulos-dropdown').value;
 
-	const urlVersiculos = 'http://www.mhj.kinghost.net:21137/versiculos/versiculo/' + cap;
+	const urlVersiculos = 'http://www.mhj.kinghost.net:21137/versiculos/versiculo/' + liv + '/' + cap;
 
 	const requestVersiculos = new XMLHttpRequest();
 	requestVersiculos.open('GET', urlVersiculos, true);
 
-	requestVersiculos.onload = function() {
-	  if (requestVersiculos.status === 200) {
-          
-          const data = JSON.parse(requestVersiculos.responseText);		
-		  console.log(data[0].qtde_versiculos);
-          var qtdeVersiculos = data[0].qtde_versiculos;
-          let option; 
-          
-          for (let i = 1; i <= qtdeVersiculos; i++) {
-              option = document.createElement('option');
-              option.text = i;
-              option.value = i;
-							versiculosDeDropdown.add(option);
-							versiculosAteDropdown.add(option);
-          }
-	   } else {
-		// Reached the server, but it returned an error
-	  }   
+	requestVersiculos.onload = function () {
+		if (requestVersiculos.status === 200) {
+
+			const data = JSON.parse(requestVersiculos.responseText);
+			console.log(data[0].qtde_versiculos);
+			var qtdeVersiculos = data[0].qtde_versiculos;
+			let option;
+
+			for (let i = 1; i <= qtdeVersiculos; i++) {
+				option = document.createElement('option');
+				option.text = i;
+				option.value = i;
+				versiculosDeDropdown.add(option);
+				versiculosAteDropdown.add(option);
+			}
+		} else {
+			// Reached the server, but it returned an error
+		}
 	}
 
-	requestVersiculos.onerror = function() {
-	  console.error('An error occurred fetching the JSON from ' + urlVersiculos);
+	requestVersiculos.onerror = function () {
+		console.error('An error occurred fetching the JSON from ' + urlVersiculos);
 	};
 
 	requestVersiculos.send();
