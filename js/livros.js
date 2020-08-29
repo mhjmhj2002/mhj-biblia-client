a7e0f625f17846d073de5a62799568c262a5f92b
Mhj <mhjmhj2002@gmail.com>
2020-08-29 17:08:10 -0300

    add ajuste client livro

--- js/livros.js
+++ js/livros.js
@@ -13,7 +13,19 @@ function carregarComboLivros(){
 
 	const urlLivros = url + ':' + port + '/livros/livro/' + document.getElementById('testamentos-dropdown').value;
 
-	const request = new XMLHttpRequest();
+	$.ajax({
+		url: urlLivros
+	  }).then(function(data) {
+		let option;
+		for (let i = 0; i < data.length; i++) {
+		  option = document.createElement('option');
+		  option.text = data[i].liv_nome;
+		  option.value = data[i].liv_id;
+		  livrosDropdown.add(option);
+		}
+	  });
+
+	/*const request = new XMLHttpRequest();
 	request.open('GET', urlLivros, true);
 	request.withCredentials = true;
 	
@@ -37,7 +49,7 @@ function carregarComboLivros(){
 	  console.error('An error occurred fetching the JSON from ' + urlLivros);
 	};
 
-	request.send();
+	request.send();*/
 	
 }
 
